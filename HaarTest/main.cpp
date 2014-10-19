610dc43aa39105e8ca256e7ee95daedc8c3f0c7c
SnqKe <julien.vogel@gmail.com>
2014-10-19 16:10:53 +0200

    Multiple iterations working and some code cleaning - code separated in functions - we can now apply many DWT iterations

--- HaarTest/main.cpp
+++ HaarTest/main.cpp
@@ -1,79 +1,88 @@
 #include <QImage>
-#include <QDebug>
+
+void waveletsTransform(int** mat, int width, int height)
+{
+    /* Le vecteur de calcul aura la taille de la plus grande valeur entre width et height pour éviter d'allouer 2 vecteurs */
+    int vec[(width > height) ? width : height]; // Passer en float
+
+    /* Opérations sur les lignes */
+    for(int j = 0; j < height; j++)
+    {
+        for(int i = 0; i < width/2 ; i++)
+        {
+            vec[i] = (mat[2*i][j] + mat[2*i+1][j])/2;
+            vec[i+width/2] = (mat[2*i][j] - mat[2*i+1][j])/2;
+        }
+        for(int i = 0; i < width; i++)
+        {
+            mat[i][j] = vec[i];
+        }
+    }
+
+    /* Opérations sur les colonnes */
+    for(int i = 0; i < width; i++)
+    {
+        for(int j = 0; j < height/2 ; j++)
+        {
+            vec[j] = (mat[i][2*j] + mat[i][2*j+1])/2;
+            vec[j+height/2] = (mat[i][2*j] - mat[i][2*j+1])/2;
+        }
+        for(int j = 0; j < height; j++)
+        {
+            mat[i][j] = vec[j];
+        }
+    }
+}
+
+void saveImage(int** mat, QImage img, int iteration)
+{
+
+    /* On modifie l'image à partir de la matrice d'entrée */
+    for(int j = 0; j < img.height(); j++)
+    {
+        for(int i = 0; i < img.width(); i++)
+        {
+            if(mat[i][j] < 0) img.setPixel(i, j, -mat[i][j]);
+            else img.setPixel(i, j, mat[i][j]);
+        }
+    }
+
+    /* Et on la sauvegarde */
+    img.save("final" + QString::number(iteration) + ".jpg", 0);
+}
 
 int main()
 {
-    QImage t("lenna"); // Image sur laquelle on travaille
-    int i, j; // Itérateurs
-    int width = t.width();
-    int height = t.height();
-    int V[width];
-    int M[width][height];
-    int w;
+    QImage img("lenna"); // Image sur laquelle on travaille
+    int width = img.width();
+    int height = img.height();
+    int** mat; // Passer en float
 
-    for(j = 0; j < height; j++) // On remplit la matrice d'entiers M avec les niveaux de gris de l'image
+    mat = (int**) malloc(sizeof(int*)*width);
+    for(int i = 0; i < width; i++)
     {
-        for(i = 0; i < width; i++)
+        mat[i] = (int*) malloc(sizeof(int)*height);
+    }
+
+    for(int j = 0; j < height; j++) // On remplit la matrice d'entiers M avec les niveaux de gris de l'image
+    {
+        for(int i = 0; i < width; i++)
         {
-            M[i][j] = qGray(t.pixel(i,j));
-            //M[i][j] = qGray(t.color(t.pixelIndex(i,j)));
+            mat[i][j] = qGray(img.pixel(i,j));
+            //mat[i][j] = qGray(t.color(img.pixelIndex(i,j)));
         }
     }
 
-    for(j = 0; j < height; j++) // Opérations sur les lignes
+    for(int i = 1; i < 4; i++, width/=2, height/=2)
     {
-        w = width;
-        //do
-        //{
-            for(i = 0; i < w/2 ; i++)
-            {
-                V[i] = (M[2*i][j] + M[2*i+1][j])/2;
-                V[i+w/2] = (M[2*i][j] - M[2*i+1][j])/2;
-            }
-            for(i = 0; i < width; i++)
-            {
-                M[i][j] = V[i];
-            }
-            //w = w/2;
-        //} while(w != 1);
+        waveletsTransform(mat, width, height);
+        saveImage(mat, img, i);
     }
 
-    for(i = 0; i < width; i++) // Opérations sur les colonnes
-    {
-        w = height;
-        //do
-        //{
-            for(j = 0; j < w/2 ; j++)
-            {
-                V[j] = (M[i][2*j] + M[i][2*j+1])/2;
-                V[j+w/2] = (M[i][2*j] - M[i][2*j+1])/2;
-            }
-            for(j = 0; j < height; j++)
-            {
-                M[i][j] = V[j];
-            }
-            //w = w/2;
-        //} while(w != 1);
-    }
-
-    for(j = 0; j < height; j++) // On enlève les valeurs négatives
-    {
-        for(i = 0; i < width; i++)
-        {
-            if(M[i][j] < 0)
-                M[i][j] = 0;
-        }
-    }
-
-    for(j = 0; j < height; j++) // On modifie l'image de départ avec notre matrice M transformée
-    {
-        for(i = 0; i < width; i++)
-        {
-            t.setPixel(i, j, M[i][j]);
-        }
-    }
-
-    t.save("final.jpg", 0); // Et on la sauvegarde
+    free(mat);
 
     return 0;
 }
+
+// TODO : Convertir en float
+// TODO : Affichage résultat semblable à celui de TER
